4149182da93abee2197a2173e1ba964702b9d90a
tarik <asdasdsa@mail.com>
2022-08-05 09:49:21 +0300

    render

--- src/screens/ProductList/index.js
+++ src/screens/ProductList/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; //dropdown
+import React, { useCallback, useEffect, useState } from 'react'; //dropdown
 import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { ScrollView } from 'react-native-gesture-handler';
@@ -9,20 +9,16 @@ const ProductList = ({ navigation }) => {
   const [userData, setUserData] = useState([]);
   const [lastDocument, setLastDocument] = useState();
 
-  function LoadData(order = 'asc') {
-    let query = userCollection.orderBy('price', order); //.where('color', 'in', ['Casper', 'red']);
-    if (lastDocument !== undefined) {
-      query = query.startAfter(lastDocument); // fetch data following the last document accessed
-    }
-    query
-      //.limit(3)
-      .get()
-      .then(querySnapshot => {
-        setLastDocument(querySnapshot.docs[querySnapshot.docs.length - 1]);
-        MakeUserData(querySnapshot.docs);
-        //func();
-      });
-  }
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('asc');
+  const [items, setItems] = useState([
+    { label: 'Increasing order of price', value: 'asc' },
+    { label: 'Decreasing order of price', value: 'desc' },
+  ]);
+
+  useEffect(() => {
+    LoadData(value);
+  }, [LoadData, value]);
 
   function reverseString(str) {
     var splitString = str.split('');
@@ -31,48 +27,65 @@ const ProductList = ({ navigation }) => {
     return joinArray;
   }
 
-  function updateProduct(doc) {
-    console.log('Product updated!');
-    userCollection.doc(doc.id).update({
-      brand: reverseString(doc._data.brand),
-    });
-    LoadData(value);
-  }
+  const LoadData = useCallback(
+    (order = 'asc') => {
+      let query = userCollection.orderBy('price', order); //.where('color', 'in', ['Casper', 'red']);
+      if (lastDocument !== undefined) {
+        query = query.startAfter(lastDocument); // fetch data following the last document accessed
+      }
+      query
+        //.limit(3)
+        .get()
+        .then(querySnapshot => {
+          setLastDocument(querySnapshot.docs[querySnapshot.docs.length]);
+          MakeUserData(querySnapshot.docs);
+          //func();
+        });
+    },
+    [MakeUserData, lastDocument],
+  );
 
-  function MakeUserData(docs) {
-    let templist = []; //[...userData] <- use this instead of [] if you want to save the previous data.
-    docs.forEach((doc, i) => {
-      console.log(doc._data);
-      let temp = (
-        <View key={i} style={styles.product}>
-          <TouchableOpacity
-            onPress={() => {
-              updateProduct(doc);
-            }}>
-            <Image
-              style={styles.imgStyle}
-              source={{
-                uri: doc._data.imgUrl,
-              }}
-            />
-            <Text>
-              {doc._data.brand} {doc._data.name} {doc._data.color}
-            </Text>
-          </TouchableOpacity>
-          <Text>{doc._data.price} TRY</Text>
-        </View>
-      );
-      templist.push(temp);
-    });
-    setUserData(templist); //replace with the new data
-  }
+  const updateProduct = useCallback(
+    doc => {
+      console.log('Product updated!');
+      userCollection.doc(doc.id).update({
+        brand: reverseString(doc._data.brand),
+      });
+      LoadData(value);
+    },
+    [LoadData, value],
+  );
 
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('asc');
-  const [items, setItems] = useState([
-    { label: 'Increasing order', value: 'asc' },
-    { label: 'Decreasing order', value: 'desc' },
-  ]);
+  const MakeUserData = useCallback(
+    docs => {
+      let templist = []; //[...userData] <- use this instead of [] if you want to save the previous data.
+      docs.forEach((doc, i) => {
+        console.log(doc._data);
+        let temp = (
+          <View key={i} style={styles.product}>
+            <TouchableOpacity
+              onPress={() => {
+                updateProduct(doc);
+              }}>
+              <Image
+                style={styles.imgStyle}
+                source={{
+                  uri: doc._data.imgUrl,
+                }}
+              />
+              <Text>
+                {doc._data.brand} {doc._data.name} {doc._data.color}
+              </Text>
+            </TouchableOpacity>
+            <Text>{doc._data.price} TRY</Text>
+          </View>
+        );
+        templist.push(temp);
+      });
+      setUserData(templist); //replace with the new data
+    },
+    [updateProduct],
+  );
 
   return (
     <View style={styles.main}>
@@ -90,15 +103,6 @@ const ProductList = ({ navigation }) => {
       <ScrollView>
         <View style={styles.products}>{userData}</View>
       </ScrollView>
-      <View>
-        <TouchableOpacity
-          style={styles.touchableOpacity}
-          onPress={() => {
-            LoadData(value);
-          }}>
-          <Text style={styles.touchableOpacityText}>Load Products</Text>
-        </TouchableOpacity>
-      </View>
     </View>
   );
 };
@@ -106,25 +110,38 @@ const ProductList = ({ navigation }) => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    paddingTop: 20,
+    paddingTop: 10,
     paddingHorizontal: 24,
-    paddingBottom: 50,
+    paddingBottom: 70,
     backgroundColor: '#F5F8FA',
   },
   product: {
     padding: 8,
-    height: '27%',
+    height: '32%',
     margin: 3,
     width: '46%',
     backgroundColor: '#E1E8ED',
     borderRadius: 10,
   },
   products: {
-    height: '70%',
+    height: '60%',
     flexDirection: 'row',
-
     flexWrap: 'wrap',
   },
+  imgStyle: {
+    width: '80%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  dropDown: {
+    width: '48%',
+    alignSelf: 'flex-end',
+    paddingBottom: 10,
+  },
+});
+export default ProductList;
+//style={{ color: doc._data.color.trim() }}
+/*
   touchableOpacity: {
     width: '45%',
     alignItems: 'center',
@@ -140,16 +157,4 @@ const styles = StyleSheet.create({
     fontSize: 17,
     padding: 15,
   },
-  imgStyle: {
-    width: '80%',
-    aspectRatio: 1,
-    alignSelf: 'center',
-  },
-  dropDown: {
-    width: '48%',
-    alignSelf: 'flex-end',
-    paddingBottom: 20,
-  },
-});
-export default ProductList;
-//style={{ color: doc._data.color.trim() }}
+  */
